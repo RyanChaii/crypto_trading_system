759826f63275b75fe2f35e3077dcffbaa9a2f675
RyanChaii <ryanchaihengee@gmail.com>
2025-06-05 20:46:26 +0800

    feat: Enable retrieving of all user's wallet
    1. Created get request for retrieving of wallets that will accept a userId header
    2. Created DTO for wallet response
    3. Created a wallet service that will pull user's wallet
    4. Created userId validation

--- crypto-service/src/main/java/com/personal/crypto/system/crypto_service/controller/WalletController.java
+++ crypto-service/src/main/java/com/personal/crypto/system/crypto_service/controller/WalletController.java
@@ -1,8 +1,14 @@
 package com.personal.crypto.system.crypto_service.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.personal.crypto.system.crypto_service.model.dto.WalletResponse;
 import com.personal.crypto.system.crypto_service.service.WalletService;
 
 @RestController
@@ -14,4 +20,11 @@ public class WalletController {
     public WalletController(WalletService walletService){
         this.walletService = walletService;
     }
+
+    @GetMapping("/retrievewallets")
+    public ResponseEntity<List<WalletResponse>> getWallets(@RequestHeader String userId) {
+        walletService.getUserWallets(userId);
+        return ResponseEntity.ok(walletService.getUserWallets(userId));
+    }
+
 }
--- crypto-service/src/main/java/com/personal/crypto/system/crypto_service/service/WalletService.java
+++ crypto-service/src/main/java/com/personal/crypto/system/crypto_service/service/WalletService.java
@@ -1,11 +1,54 @@
 package com.personal.crypto.system.crypto_service.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+
+import com.personal.crypto.system.crypto_service.model.dto.WalletResponse;
+import com.personal.crypto.system.crypto_service.model.entity.UserWallet;
+import com.personal.crypto.system.crypto_service.repository.UserWalletRepository;
 
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
 public class WalletService {
-    
+
+    private final UserWalletRepository userWalletRepository;
+
+    public WalletService(UserWalletRepository userWalletRepository) {
+        this.userWalletRepository = userWalletRepository;
+    }
+
+    public List<WalletResponse> getUserWallets(String userId) {
+
+        userIdValidation(userId);
+
+        List<UserWallet> allWallets = userWalletRepository.findByUserId(userId);
+
+        if (allWallets.isEmpty()) {
+            throw new IllegalArgumentException("No wallet found for user: " + userId);
+        }
+
+        List<WalletResponse> retrievedResults = allWallets.stream()
+            .map(w -> new WalletResponse(w.getCryptoCurrency(), w.getWalletBalance()))
+            .collect(Collectors.toList());
+
+        
+
+        return retrievedResults;
+    }
+
+    public void userIdValidation (String userId) {
+        // Validate userId
+        if (userId.isBlank()) {
+            throw new IllegalArgumentException("User ID is required");
+        }
+
+        if (!userWalletRepository.existsByUserId(userId)) {
+            throw new IllegalArgumentException("User not found");
+        }
+
+    }
 }
