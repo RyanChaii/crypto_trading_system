1f9577bec406dcf1769f04b5d3c92880aefb3ef9
RyanChaii <ryanchaihengee@gmail.com>
2025-06-05 14:12:07 +0800

    feat: Created api to retrieve best price
    1. Best price api are now working
    2. Completed method at service that will compare prices between 2 sources and retrieve the best result
    3. Added custom port 2025
    4. Added additional note to differentiate between best bid and ask price

--- crypto-service/src/main/java/com/personal/crypto/system/crypto_service/controller/PriceController.java
+++ crypto-service/src/main/java/com/personal/crypto/system/crypto_service/controller/PriceController.java
@@ -1,8 +1,13 @@
 package com.personal.crypto.system.crypto_service.controller;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.personal.crypto.system.crypto_service.model.dto.AggregatedPriceResponse;
 import com.personal.crypto.system.crypto_service.service.PriceService;
 
 @RestController
@@ -13,6 +18,11 @@ public class PriceController {
 
     public PriceController(PriceService priceService) {
     this.priceService = priceService;
+    }
+
+    @GetMapping("/aggregatedprice")
+    public ResponseEntity<Map<String, AggregatedPriceResponse>> getAggregatedPrices() {
+        return ResponseEntity.ok(priceService.getBestAggregatedPrices());
     }
 
 }
--- crypto-service/src/main/java/com/personal/crypto/system/crypto_service/service/PriceService.java
+++ crypto-service/src/main/java/com/personal/crypto/system/crypto_service/service/PriceService.java
@@ -1,7 +1,15 @@
 package com.personal.crypto.system.crypto_service.service;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import com.personal.crypto.system.crypto_service.model.dto.AggregatedPriceResponse;
+import com.personal.crypto.system.crypto_service.model.entity.AggregatedPrice;
 import com.personal.crypto.system.crypto_service.repository.AggregatedPriceRepository;
 
 @Service
@@ -11,5 +19,41 @@ public class PriceService {
 
     public PriceService(AggregatedPriceRepository aggregatedPriceRepository) {
         this.aggregatedPriceRepository = aggregatedPriceRepository;
+    }
+
+    public Map<String, AggregatedPriceResponse> getBestAggregatedPrices() {
+        // Retrieve data from repo
+        List<AggregatedPrice> prices = aggregatedPriceRepository.findAll();
+
+        // Group by pair type and calculate best bid/ask
+        Map<String, AggregatedPriceResponse> bestResult = new HashMap<>();
+
+        // loop thru and get btcusdt first process it and then ethusdt
+        for (String pairType : List.of("btcusdt", "ethusdt")) {
+            // Stream thru each result that match each list item above
+            List<AggregatedPrice> pairTypePrices = prices.stream()
+                .filter(p -> pairType.equalsIgnoreCase(p.getPairType()))
+                .toList();
+
+            // deal with all bit then eth, 
+            // return the best bid
+            // the best price a buyer offers to buy at, good for sellers because they earn more, hence max
+            BigDecimal bestBid = pairTypePrices.stream()
+                .map(AggregatedPrice::getBidPrice)
+                .max(Comparator.naturalOrder())
+                .orElse(BigDecimal.ZERO);
+
+            // return the best ask
+            // the best price a seller offers to sell at, good for buyers because they pay less, hence min
+            BigDecimal bestAsk = pairTypePrices.stream()
+                .map(AggregatedPrice::getAskPrice)
+                .min(Comparator.naturalOrder())
+                .orElse(BigDecimal.ZERO);
+            
+            bestResult.put(pairType, new AggregatedPriceResponse(pairType, bestBid, bestAsk));
+        }
+
+        return bestResult;
+
     }
 }
